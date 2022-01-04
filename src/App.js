0fef31989cb0d3542dd8063ba7134406a675d611
daying.zheng <daying.zheng@geely.com>
2022-01-04 18:39:00 +0800

    feat:add layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import  MyRouter  from './router/index'; 
+import BaseLayout from './pages/layout'
 import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <MyRouter/>
+      <BaseLayout/>
     </BrowserRouter>
   );
 }
